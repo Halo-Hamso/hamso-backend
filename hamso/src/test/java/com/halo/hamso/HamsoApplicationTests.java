a7a9e14d42938e16f9c49ebf6b285eecae5a4ae1
Ahnheechan <ahc70032@gmail.com>
2023-10-05 23:43:15 +0900

    init repo
    1. application.properties 삭제
    2. git ignore에 application.yml 추가

---
+++ hamso/src/test/java/com/halo/hamso/HamsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halo.hamso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
